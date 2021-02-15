bea5bff6411df2b21d84133c234c01a02c9bb98e
Howarddj <66848380+Howarddj@users.noreply.github.com>
2021-02-15 16:09:36 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,4 +88,4 @@ TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
 
 # Workaround for error copying vendor files to recovery ramdisk
-#TARGET_COPY_OUT_VENDOR := system/vendor
+TARGET_COPY_OUT_VENDOR := system/vendor
