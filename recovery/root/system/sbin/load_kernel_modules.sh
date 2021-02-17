f759542b12925b67e8c501a44a886ad76127fee7
Howarddj <66848380+Howarddj@users.noreply.github.com>
2021-02-17 18:50:25 +0000

    Add files via upload

---
+++ recovery/root/system/sbin/load_kernel_modules.sh
@@ -0,0 +1,7 @@
+#!/sbin/sh
+
+SLOT=`getprop ro.boot.slot_suffix`
+mount /dev/block/bootdevice/by-name/vendor$SLOT /vendor -o ro
+cp /vendor/lib/modules/nt36xxx_driver.ko /system/sbin
+umount /vendor
+insmod /system/sbin/nt36xxx_driver.ko
