3ffe66d86068219d542699f886a9e3815b7074b4
Howarddj <66848380+Howarddj@users.noreply.github.com>
2021-02-27 16:46:56 +0000

    Delete recovery directory

--- recovery/root/system/sbin/load_kernel_modules.sh
+++
@@ -1,7 +0,0 @@
-#!/sbin/sh
-
-SLOT=`getprop ro.boot.slot_suffix`
-mount /dev/block/bootdevice/by-name/vendor$SLOT /vendor -o ro
-cp /vendor/lib/modules/nt36xxx_driver.ko /system/sbin
-umount /vendor
-insmod /system/sbin/nt36xxx_driver.ko
